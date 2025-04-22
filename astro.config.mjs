322ce944e31d6b3342bbe321b9b7473ada0ef59d
Artur Galstyan <mail@arturgalstyan.dev>
2025-04-22 18:35:39 +0200

    other version

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,7 +21,7 @@ export default defineConfig({
   output: "server",
   adapter: vercel({
     webAnalytics: {
-      enabled: true, // set to false when using @vercel/analytics@1.4.0
+      enabled: true,
     },
   }),
 });
