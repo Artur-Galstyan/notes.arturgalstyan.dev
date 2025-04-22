1ab346f47bb0b2329359c664401b24edefd85bf0
Artur Galstyan <mail@arturgalstyan.dev>
2025-04-22 18:19:36 +0200

    first

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+    shikiConfig: {
+      themes: { light: "min-light", dark: "night-owl" },
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
