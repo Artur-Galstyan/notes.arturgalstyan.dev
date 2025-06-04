30d3ba0c6245f97c9c258f0a34d1c8f48169e969
Artur Galstyan <mail@arturgalstyan.dev>
2025-06-05 00:20:17 +0200

    back adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import tailwindcss from "@tailwindcss/vite";
+import vercelStatic from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,4 +18,10 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: vercelStatic({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  output: "static",
 });
